22a169a9d39c94f010f473d91da8f78d171d9655
Chris Preston <chris_preston@waters.com>
2017-10-04 13:37:16 +0100

    Now actually posts to our slack

--- mabbot.py
+++ mabbot.py
@@ -1,3 +1,5 @@
+import json
+import requests
 from random import choice, randrange
 
 def random_consonant():
@@ -10,8 +12,11 @@ def random_syllable():
     return random_consonant() + random_vowel()
 
 if __name__ == '__main__':
-    bio = ""
+    antibody = ""
     for x in range(0, randrange(2,5)):
-        bio += random_syllable()
-    bio += choice([random_consonant()+'umab','zumab','ximab',random_consonant()+'omab'])
-    print(bio)
+        antibody += random_syllable()
+    antibody += choice([random_consonant() + 'umab', 'zumab', 'ximab', random_consonant() + 'omab'])
+    payload = {"text": "_Antibody of the day:_ *" + antibody + "*", "username": 'mabbot'}
+    r = requests.post('https://hooks.slack.com/services/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',
+                      data=json.dumps(payload),
+                      headers={'Content-Type': 'application/json'})
