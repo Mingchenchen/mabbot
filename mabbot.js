5eda3383685dd4dd7a2cfa73c8bf245329338b88
Chris Preston <chris_preston@waters.com>
2017-10-06 12:20:25 +0100

    Ported to JS rather than Python

---
+++ mabbot.js
@@ -0,0 +1,33 @@
+let request = require('request');
+
+function random_choice(options) {
+    return options[Math.floor(Math.random()*options.length)];
+}
+
+function random_vowel() {
+    let vowels = 'aeiou';
+    return random_choice(vowels.split(''));
+}
+
+function random_consonant() {
+    let consonants = 'bcdfghjklmnprstvwxyz';
+    return random_choice(consonants.split(''));
+}
+
+function random_syllable() {
+    return random_consonant() + random_vowel();
+}
+
+let antibody = "";
+let number_of_parts = Math.floor(Math.random() * 2) + 2;
+for (let i = 0; i < number_of_parts; i++) {
+    antibody += random_syllable();
+}
+antibody += random_choice([random_consonant() + 'umab', 'zumab', 'ximab', random_consonant() + 'omab']);
+let payload = {"text": "_Antibody of the day:_ *" + antibody + "*", "username": 'mabbot'}
+request({
+    url: 'https://hooks.slack.com/services/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',
+    method: "POST",
+    json: true,
+    body: payload
+});
