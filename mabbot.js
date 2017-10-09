ac816acc8d63d8578de08fe9c8de305814397ce1
Chris Preston <chris_preston@waters.com>
2017-10-09 14:04:01 +0100

    Now using Mal's improved consonant list from https://gist.github.com/malross/57287af9ec80563ae8d12a14d27d1020

--- mabbot.js
+++ mabbot.js
@@ -9,9 +9,45 @@ function random_vowel() {
     return random_choice(vowels.split(''));
 }
 
-function random_consonant() {
-    let consonants = 'bcdfghjklmnprstvwxyz';
-    return random_choice(consonants.split(''));
+function random_consonant() { 
+    // Taken from https://gist.github.com/malross/57287af9ec80563ae8d12a14d27d1020
+    let consonants = [
+        "b",
+        "bl",
+        "br",
+        "c",
+        "ch",
+        "cl",
+        "cr",
+        "d",
+        "dr",
+        "f",
+        "fl",
+        "fr",
+        "g",
+        "gl",
+        "gr",
+        "k",
+        "kr",
+        "l",
+        "m",
+        "n",
+        "p",
+        "ph",
+        "pl",
+        "pr",
+        "r",
+        "s",
+        "sh",
+        "st",
+        "t",
+        "th",
+        "tr",
+        "v",
+        "x",
+        "z"
+    ];
+    return random_choice(consonants);
 }
 
 function random_syllable() {
@@ -24,7 +60,7 @@ for (let i = 0; i < number_of_parts; i++) {
     antibody += random_syllable();
 }
 antibody += random_choice([random_consonant() + 'umab', 'zumab', 'ximab', random_consonant() + 'omab']);
-let payload = {"text": "_Antibody of the day:_ *" + antibody + "*", "username": 'mabbot'}
+let payload = {"text": "_Antibody of the day:_ *" + antibody + "*", "username": 'mabbot'};
 request({
     url: 'https://hooks.slack.com/services/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',
     method: "POST",
